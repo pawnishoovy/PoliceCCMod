417efa8876a303ffe2bce0e4ee643c18431d9a33
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2020-12-05 13:30:18 +0100

    disable weirdo explosion smoke

--- Heat.rte/Devices/Weapons/Handheld/Disabler/Bullet.lua
+++ Heat.rte/Devices/Weapons/Handheld/Disabler/Bullet.lua
@@ -2,62 +2,71 @@ function Update(self)
 	
 	if math.random(100) > 93 then
 		local Effect = CreateMOPixel("Spark Yellow 1")
-		if Effect then
-			Effect.Pos = self.Pos
-			Effect.Vel = ((self.Vel / 2) + Vector(RangeRand(-20,20), RangeRand(-20,20))) / 30
-			MovableMan:AddParticle(Effect)
-		end
+		Effect.Pos = self.Pos
+		Effect.Vel = ((self.Vel / 2) + Vector(RangeRand(-20,20), RangeRand(-20,20))) / 30
+		MovableMan:AddParticle(Effect)
 	end
 
 	if math.random(100) > 98 then
 		local Effect = CreateMOPixel("Spark Yellow 2")
-		if Effect then
-			Effect.Pos = self.Pos
-			Effect.Vel = ((self.Vel / 2) + Vector(RangeRand(-20,20), RangeRand(-20,20))) / 30
-			MovableMan:AddParticle(Effect)
-		end
+		Effect.Pos = self.Pos
+		Effect.Vel = ((self.Vel / 2) + Vector(RangeRand(-20,20), RangeRand(-20,20))) / 30
+		MovableMan:AddParticle(Effect)
 	end
 
 end
 
+-- ?????
 
-function Destroy(self)
-	local Offset = self.Vel*(8*TimerMan.DeltaTimeSecs)
-
-
+-- function Destroy(self)
 	
-	for i = 1, 1 do
-		local Effect = CreateMOSParticle("Tiny Smoke Ball 1", "Base.rte")
-		if Effect then
-			Effect.Pos = self.Pos + Offset
-			Effect.Vel = ((self.Vel*-1) + Vector(RangeRand(-100,100), RangeRand(-10,10))) / 10
-			MovableMan:AddParticle(Effect)
-		end
-	end
+	-- local endVector = Vector();
+	-- local rayResult = SceneMan:CastObstacleRay(self.Pos, self.Vel, endVector, endVector, self.ID, -1, rte.grassID, 1)
+	-- local vel = Vector(self.Vel.X, self.Vel.Y)
 	
-	for i = 1, 1 do
-		local Effect = CreateMOSParticle("Side Thruster Blast Ball 1", "Base.rte")
-		if Effect then
-			Effect.Pos = self.Pos + Offset
-			Effect.Vel = ((self.Vel*-1) + Vector(RangeRand(-100,100), RangeRand(-20,20))) / 10
-			MovableMan:AddParticle(Effect)
-		end
-	end
+	-- rayResult = rayResult - 1;
+	-- print(rayResult)
+	-- if rayResult < 0 then
+		-- rayResult = 0;
+	-- end
+	
+	-- print(rayResult)
+	-- print(vel)
+	-- PrimitiveMan:DrawCirclePrimitive(self.Pos, 8, 5)
+	
+	-- vel:SetMagnitude(rayResult)
+	
+	-- if self.Vel.Magnitude < 3 then
+		-- endVector = self.Pos;
+	-- else
+		-- endVector = self.Pos + vel;
+	-- end
 
-	if math.random(100) > 99 then
-		local Effect = CreateMOSParticle("Explosion Smoke 2 Glow")
-		if Effect then
-			Effect.Pos = self.Pos + Offset
-			Effect.Vel = ((self.Vel*-1) + Vector(RangeRand(-20,20), RangeRand(-20,20))) / 10
-			MovableMan:AddParticle(Effect)
-		end
-	end
-	if math.random(100) > 99 then
-		local Effect = CreateMOSParticle("Explosion Smoke 1")
-		if Effect then
-			Effect.Pos = self.Pos + Offset
-			Effect.Vel = ((self.Vel*-1) + Vector(RangeRand(-20,20), RangeRand(-20,20))) / 10
-			MovableMan:AddParticle(Effect)
-		end
-	end
-end
+	-- for i = 1, 1 do
+		-- local Effect = CreateMOSParticle("Tiny Smoke Ball 1", "Base.rte")
+		-- Effect.Pos = endVector
+		-- Effect.Vel = ((self.Vel*-1) + Vector(RangeRand(-100,100), RangeRand(-10,10))) / 10
+		-- MovableMan:AddParticle(Effect)
+	-- end
+	
+	-- for i = 1, 3 do
+		-- local Effect = CreateMOSParticle("Side Thruster Blast Ball 1", "Base.rte")
+		-- Effect.Pos = endVector
+		-- Effect.Vel = ((self.Vel*-1) + Vector(RangeRand(-100,100), RangeRand(-20,20))) / 10
+		-- MovableMan:AddParticle(Effect)
+	-- end
+
+	-- if math.random(100) > 85 then
+		-- local Effect = CreateMOSParticle("Explosion Smoke 2 Glow")
+		-- Effect.Pos = endVector
+		-- Effect.Vel = ((self.Vel*-1) + Vector(RangeRand(-20,20), RangeRand(-20,20))) / 10
+		-- MovableMan:AddParticle(Effect)
+	-- end
+	-- if math.random(100) > 70 then
+		-- local Effect = CreateMOSParticle("Explosion Smoke 1")
+		-- Effect.Pos = endVector
+		-- Effect.Vel = ((self.Vel*-1) + Vector(RangeRand(-20,20), RangeRand(-20,20))) / 10
+		-- MovableMan:AddParticle(Effect)
+	-- end
+
+-- end
