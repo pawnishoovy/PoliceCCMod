a3788b1a847a2965b0201dfd4d11abf94f97d4bf
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2021-01-05 16:05:37 +0100

    make AI not use footpixels

--- Heat.rte/Actors/Shared/HeatAIBehaviours.lua
+++ Heat.rte/Actors/Shared/HeatAIBehaviours.lua
@@ -81,56 +81,75 @@ function HeatAIBehaviours.handleMovement(self)
 	local crouching = self.controller:IsState(Controller.BODY_CROUCH)
 	local moving = self.controller:IsState(Controller.MOVE_LEFT) or self.controller:IsState(Controller.MOVE_RIGHT);
 	
-	--NOTE: you could also put in things like your falling scream here easily with very little overhead
-	
 	-- Leg Collision Detection system
     --local i = 0
-    for i = 1, 2 do
-        --local foot = self.feet[i]
-		local foot = nil
-        --local leg = self.legs[i]
-		if i == 1 then
-			foot = self.FGFoot 
+	if self:IsPlayerControlled() then -- AI doesn't update its own foot checking when playercontrolled so we have to do it
+		if self.Vel.Y > 10 then
+			self.wasInAir = true;
 		else
-			foot = self.BGFoot 
+			self.wasInAir = false;
 		end
-        --if foot ~= nil and leg ~= nil and leg.ID ~= rte.NoMOID then
-		if foot ~= nil then
-            local footPos = foot.Pos				
-			local mat = nil
-			local pixelPos = footPos + Vector(0, 4)
-			self.footPixel = SceneMan:GetTerrMatter(pixelPos.X, pixelPos.Y)
-			--PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 13)
-			if self.footPixel ~= 0 then
-				mat = SceneMan:GetMaterialFromID(self.footPixel)
-			--	PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 162);
-			--else
-			--	PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 13);
-			end
-			
-			local movement = (self.controller:IsState(Controller.MOVE_LEFT) == true or self.controller:IsState(Controller.MOVE_RIGHT) == true or self.Vel.Magnitude > 3)
-			if mat ~= nil then
-				--PrimitiveMan:DrawTextPrimitive(footPos, mat.PresetName, true, 0);
-				if self.feetContact[i] == false then
-					self.feetContact[i] = true
-					if self.feetTimers[i]:IsPastSimMS(self.footstepTime) and movement then																	
-						self.feetTimers[i]:Reset()
-					end
-				end
+		for i = 1, 2 do
+			--local foot = self.feet[i]
+			local foot = nil
+			--local leg = self.legs[i]
+			if i == 1 then
+				foot = self.FGFoot 
 			else
-				if self.feetContact[i] == true then
-					self.feetContact[i] = false
-					if self.feetTimers[i]:IsPastSimMS(self.footstepTime) and movement then
-						self.feetTimers[i]:Reset()
+				foot = self.BGFoot 
+			end
+
+			--if foot ~= nil and leg ~= nil and leg.ID ~= rte.NoMOID then
+			if foot ~= nil then
+				local footPos = foot.Pos				
+				local mat = nil
+				local pixelPos = footPos + Vector(0, 4)
+				self.footPixel = SceneMan:GetTerrMatter(pixelPos.X, pixelPos.Y)
+				--PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 13)
+				if self.footPixel ~= 0 then
+					mat = SceneMan:GetMaterialFromID(self.footPixel)
+				--	PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 162);
+				--else
+				--	PrimitiveMan:DrawLinePrimitive(pixelPos, pixelPos, 13);
+				end
+				
+				local movement = (self.controller:IsState(Controller.MOVE_LEFT) == true or self.controller:IsState(Controller.MOVE_RIGHT) == true or self.Vel.Magnitude > 3)
+				if mat ~= nil then
+					--PrimitiveMan:DrawTextPrimitive(footPos, mat.PresetName, true, 0);
+					if self.feetContact[i] == false then
+						self.feetContact[i] = true
+						if self.feetTimers[i]:IsPastSimMS(self.footstepTime) and movement then																	
+							self.feetTimers[i]:Reset()
+						end
+					end
+				else
+					if self.feetContact[i] == true then
+						self.feetContact[i] = false
+						if self.feetTimers[i]:IsPastSimMS(self.footstepTime) and movement then
+							self.feetTimers[i]:Reset()
+						end
 					end
 				end
+			end
+		end
+	else
+		if self.AI.flying == true and self.wasInAir == false then
+			self.wasInAir = true;
+		elseif self.AI.flying == false and self.wasInAir == true then
+			self.wasInAir = false;
+			self.isJumping = false
+			if self.moveSoundTimer:IsPastSimMS(500) then
+				self.movementSounds.Land:Play(self.Pos);
+				self.moveSoundTimer:Reset();
 			end
 		end
 	end
 	
+	
+	
 	-- Custom Jump
 	if self.controller:IsState(Controller.BODY_JUMPSTART) == true and self.controller:IsState(Controller.BODY_CROUCH) == false and self.jumpTimer:IsPastSimMS(self.jumpDelay) and not self.isJumping then
-		if self.feetContact[1] == true or self.feetContact[2] == true then
+		if (self:IsPlayerControlled() and self.feetContact[1] == true or self.feetContact[2] == true) or self.wasInAir == false then
 			local jumpVec = Vector(0,-1.5)
 			local jumpWalkX = 3
 			if self.controller:IsState(Controller.MOVE_LEFT) == true then
@@ -149,7 +168,7 @@ function HeatAIBehaviours.handleMovement(self)
 			self.jumpStop:Reset()
 		end
 	elseif self.isJumping or self.wasInAir then
-		if (self.feetContact[1] == true or self.feetContact[2] == true) and self.jumpStop:IsPastSimMS(100) then
+		if (self:IsPlayerControlled() and self.feetContact[1] == true or self.feetContact[2] == true) and self.jumpStop:IsPastSimMS(100) then
 			self.isJumping = false
 			self.wasInAir = false;
 			if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
@@ -176,6 +195,7 @@ function HeatAIBehaviours.handleMovement(self)
 		
 		if boosting and self.boosterReady then
 		
+			self.isJumping = true;
 			self.jumpJetSound:Play(self.Pos);
 			
 			self.Vel = Vector(self.Vel.X, self.Vel.Y):RadRotate(-self.RotAngle)
@@ -227,13 +247,7 @@ function HeatAIBehaviours.handleMovement(self)
 			self.moveSoundTimer:Reset();
 		end
 	end
-	
-	if self.Vel.Y > 10 then
-		self.wasInAir = true;
-	else
-		self.wasInAir = false;
-	end
-	
+
 	self.wasCrouching = crouching;
 	self.wasMoving = moving;
 end
