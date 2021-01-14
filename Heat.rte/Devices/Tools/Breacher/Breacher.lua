d1a5677fefaa92c749ebfe76f7857d738685170a
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2021-01-14 08:38:14 +0100

    simple breaching tool, just needs 1 sprite

---
+++ Heat.rte/Devices/Tools/Breacher/Breacher.lua
@@ -0,0 +1,371 @@
+function Create(self)
+	
+	-- Sounds --
+
+	self.reflectionSound = CreateSoundContainer("Reflection Breacher", "Heat.rte");
+	
+	self.chargeSound = CreateSoundContainer("Charge Breacher", "Heat.rte");
+	
+	self.flashSound = CreateSoundContainer("Flash Breacher", "Heat.rte");
+	
+	self.flashIndoorsSound = CreateSoundContainer("Flash Indoors Breacher", "Heat.rte");
+
+	self.parentSet = false;
+	
+	self.originalStanceOffset = Vector(math.abs(self.StanceOffset.X), self.StanceOffset.Y)
+	self.originalSharpStanceOffset = Vector(self.SharpStanceOffset.X, self.SharpStanceOffset.Y)
+	
+	self.rotation = 0
+	self.rotationTarget = 0
+	self.rotationSpeed = 9
+	
+	self.horizontalAnim = 0
+	self.verticalAnim = 0
+	
+	self.angVel = 0
+	self.lastRotAngle = self.RotAngle
+	
+	self.delayedFire = false
+	self.delayedFireTimer = Timer();
+	self.delayedFireTimeMS = 1700
+	self.delayedFireEnabled = true
+	
+	self.lastAge = self.Age + 0
+	
+	self.fireDelayTimer = Timer()
+	
+	self.activated = false
+	
+	local ms = 1 / (self.RateOfFire / 60) * 1000
+	ms = ms + self.delayedFireTimeMS
+	self.RateOfFire = 1 / (ms / 1000) * 60
+	
+	self.flashTimer = Timer();
+	self.flashDelay = 200;
+	self.maxFlash = 4;
+	
+	-- Progressive Recoil System 
+	self.recoilAcc = 0 -- for sinous
+	self.recoilStr = 0 -- for accumulator
+	self.recoilStrength = 30 -- multiplier for base recoil added to the self.recoilStr when firing
+	self.recoilPowStrength = 0.1 -- multiplier for self.recoilStr when firing
+	self.recoilRandomUpper = 2 -- upper end of random multiplier (1 is lower)
+	self.recoilDamping = 1.2
+	
+	self.recoilMax = 1 -- in deg.
+	self.originalSharpLength = self.SharpLength
+	-- Progressive Recoil System 
+end
+
+function Update(self)
+	self.Frame = 0;
+	self.rotationTarget = 0 -- ZERO IT FIRST AAAA!!!!!
+	
+	if self.ID == self.RootID then
+		self.parent = nil;
+		self.parentSet = false;
+	elseif self.parentSet == false then
+		local actor = MovableMan:GetMOFromID(self.RootID);
+		if actor and IsAHuman(actor) then
+			self.parent = ToAHuman(actor);
+			self.parentSet = true;
+		end
+	end
+	
+	-- Check if switched weapons/hide in the inventory, etc.
+	if self.Age > (self.lastAge + TimerMan.DeltaTimeSecs * 2000) then
+		if self.delayedFire then
+			self.delayedFire = false
+		end
+		self.fireDelayTimer:Reset()
+	end
+	self.lastAge = self.Age + 0
+	
+	local fire = self:IsActivated()
+	self:Deactivate()
+	
+	if not self.Fired == true then
+
+		if fire then
+			if not self.activated and not self.delayedFire and self.fireDelayTimer:IsPastSimMS(1 / (self.RateOfFire / 60) * 1000) then
+				self.activated = true
+				
+				self.chargeSound:Play(self.Pos);
+				
+				self.fireDelayTimer:Reset()
+				
+				self.delayedFire = true
+				self.delayedFireTimer:Reset()
+			end
+		else
+			if self.activated then
+				self.activated = false
+			end
+		end
+		
+		self.chargeSound.Pos = self.Pos;
+		
+		if self.delayedFire and self.delayedFireTimer:IsPastSimMS(self.delayedFireTimeMS) then
+			self:Activate()
+			self.delayedFire = false
+		end
+	elseif self.Flashes < self.maxFlash and self.flashTimer:IsPastSimMS(self.flashDelay) then
+		self.Flashes = self.Flashes + 1;
+		self.flashTimer:Reset();
+		
+		-- Flash
+		local ID = self.parent and self.parent.ID or 0;
+		
+		local rayVec = Vector(75 * self.FlipFactor, 0):RadRotate(self.RotAngle)
+		
+		local endPos = self.MuzzlePos + rayVec; -- This value is going to be overriden by function below, this is the end of the ray
+		local ray = SceneMan:CastObstacleRay(self.MuzzlePos, rayVec, Vector(0, 0), endPos, ID, self.Team, 0, 2) -- Do the hitscan stuff, raycast
+		--local vec = SceneMan:ShortestDistance(point,endPos,SceneMan.SceneWrapsX);
+		
+		local Effect = CreateMOPixel("Flash Glow CLIER", "Heat.rte")
+		if Effect then
+			Effect.Pos = self.MuzzlePos;
+			--Effect.Vel = (self.Vel + Vector(RangeRand(-20,20), RangeRand(-20,20)) + Vector(150*self.FlipFactor,0):RadRotate(self.RotAngle)) / 30
+			MovableMan:AddParticle(Effect)
+		end
+		
+		for i = 0, math.random(3,6) do
+			local Effect = CreateMOPixel("Flash Glow CLIER", "Heat.rte")
+			if Effect then
+				Effect.Pos = self.MuzzlePos + Vector(math.random(15,50) * self.FlipFactor, 0):RadRotate(self.RotAngle + math.rad(RangeRand(-45,45)));
+				MovableMan:AddParticle(Effect)
+			end
+		end
+		
+		local Flash = CreateMOPixel("Flash CLIER", "Heat.rte")
+		if Flash then
+			Flash.Pos = endPos;
+			Flash.Vel = Vector(45,0)
+			Flash.Team = self.Team
+			MovableMan:AddParticle(Flash)
+		end
+		
+		local outdoorRays = 0;
+		
+		local indoorRays = 0;
+		
+		local bigIndoorRays = 0;
+
+		if self.parent and self.parent:IsPlayerControlled() then
+			self.rayThreshold = 2; -- this is the first ray check to decide whether we play outdoors
+			local Vector2 = Vector(0,-700); -- straight up
+			local Vector2Left = Vector(0,-700):RadRotate(45*(math.pi/180));
+			local Vector2Right = Vector(0,-700):RadRotate(-45*(math.pi/180));			
+			local Vector2SlightLeft = Vector(0,-700):RadRotate(22.5*(math.pi/180));
+			local Vector2SlightRight = Vector(0,-700):RadRotate(-22.5*(math.pi/180));		
+			local Vector3 = Vector(0,0); -- dont need this but is needed as an arg
+			local Vector4 = Vector(0,0); -- dont need this but is needed as an arg
+
+			self.ray = SceneMan:CastObstacleRay(self.Pos, Vector2, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.rayRight = SceneMan:CastObstacleRay(self.Pos, Vector2Right, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.rayLeft = SceneMan:CastObstacleRay(self.Pos, Vector2Left, Vector3, Vector4, self.RootID, self.Team, 128, 7);			
+			self.raySlightRight = SceneMan:CastObstacleRay(self.Pos, Vector2SlightRight, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.raySlightLeft = SceneMan:CastObstacleRay(self.Pos, Vector2SlightLeft, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			
+			self.rayTable = {self.ray, self.rayRight, self.rayLeft, self.raySlightRight, self.raySlightLeft};
+		else
+			self.rayThreshold = 1; -- has to be different for AI
+			local Vector2 = Vector(0,-700); -- straight up
+			local Vector3 = Vector(0,0); -- dont need this but is needed as an arg
+			local Vector4 = Vector(0,0); -- dont need this but is needed as an arg		
+			self.ray = SceneMan:CastObstacleRay(self.Pos, Vector2, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			
+			self.rayTable = {self.ray};
+		end
+		
+		for _, rayLength in ipairs(self.rayTable) do
+			if rayLength < 0 then
+				outdoorRays = outdoorRays + 1;
+			elseif rayLength > 170 then
+				bigIndoorRays = bigIndoorRays + 1;
+			else
+				indoorRays = indoorRays + 1;
+			end
+		end
+		
+		if outdoorRays >= self.rayThreshold then
+			self.flashSound:Play(self.Pos);
+		else
+			self.flashIndoorsSound:Play(self.Pos);
+		end
+		
+		if self.Flashes == self.maxFlash then
+			self:GibThis();
+		end
+		
+	end
+	
+	if self.FiredFrame then
+		
+		self.Fired = true;
+		self.RateOfFire = 1;
+		self.flashTimer:Reset();
+		self.Flashes = 0;
+		
+		self.canSmoke = true
+		
+		for i = 1, 4 do
+			local Effect = CreateMOSParticle("Tiny Smoke Ball 1", "Base.rte")
+			if Effect then
+				Effect.Pos = self.MuzzlePos;
+				Effect.Vel = (self.Vel + Vector(RangeRand(-20,20), RangeRand(-20,20)) + Vector(150*self.FlipFactor,0):RadRotate(self.RotAngle)) / 30
+				MovableMan:AddParticle(Effect)
+			end
+		end
+		
+		for i = 1, 2 do
+			local Effect = CreateMOSParticle("Explosion Smoke 1", "Base.rte")
+			if Effect then
+				Effect.Pos = self.MuzzlePos;
+				Effect.Vel = (self.Vel + Vector(RangeRand(-20,20), RangeRand(-20,20)) + Vector(150*self.FlipFactor,0):RadRotate(self.RotAngle)) / 30
+				MovableMan:AddParticle(Effect)
+			end
+		end
+		
+		local Effect = CreateMOSParticle("Explosion Smoke 2", "Base.rte")
+		if Effect then
+			Effect.Pos = self.MuzzlePos;
+			Effect.Vel = (self.Vel + Vector(RangeRand(-20,20), RangeRand(-20,20)) + Vector(150*self.FlipFactor,0):RadRotate(self.RotAngle)) / 30
+			MovableMan:AddParticle(Effect)
+		end
+		
+		local Effect = CreateMOPixel("Glow Explosion Huge", "Base.rte")
+		if Effect then
+			Effect.Pos = self.MuzzlePos;
+			Effect.Vel = (self.Vel)
+			MovableMan:AddParticle(Effect)
+		end
+		
+		for i = 1, 4 do
+			local Effect = CreateMOSParticle("Side Thruster Blast Ball 1", "Base.rte")
+			if Effect then
+				Effect.Pos = self.MuzzlePos;
+				Effect.Vel = (self.Vel + Vector(150*self.FlipFactor,0):RadRotate(self.RotAngle)) / 10
+				MovableMan:AddParticle(Effect)
+			end
+		end
+
+		local outdoorRays = 0;
+		
+		local indoorRays = 0;
+		
+		local bigIndoorRays = 0;
+
+		if self.parent and self.parent:IsPlayerControlled() then
+			self.rayThreshold = 2; -- this is the first ray check to decide whether we play outdoors
+			local Vector2 = Vector(0,-700); -- straight up
+			local Vector2Left = Vector(0,-700):RadRotate(45*(math.pi/180));
+			local Vector2Right = Vector(0,-700):RadRotate(-45*(math.pi/180));			
+			local Vector2SlightLeft = Vector(0,-700):RadRotate(22.5*(math.pi/180));
+			local Vector2SlightRight = Vector(0,-700):RadRotate(-22.5*(math.pi/180));		
+			local Vector3 = Vector(0,0); -- dont need this but is needed as an arg
+			local Vector4 = Vector(0,0); -- dont need this but is needed as an arg
+
+			self.ray = SceneMan:CastObstacleRay(self.Pos, Vector2, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.rayRight = SceneMan:CastObstacleRay(self.Pos, Vector2Right, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.rayLeft = SceneMan:CastObstacleRay(self.Pos, Vector2Left, Vector3, Vector4, self.RootID, self.Team, 128, 7);			
+			self.raySlightRight = SceneMan:CastObstacleRay(self.Pos, Vector2SlightRight, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			self.raySlightLeft = SceneMan:CastObstacleRay(self.Pos, Vector2SlightLeft, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			
+			self.rayTable = {self.ray, self.rayRight, self.rayLeft, self.raySlightRight, self.raySlightLeft};
+		else
+			self.rayThreshold = 1; -- has to be different for AI
+			local Vector2 = Vector(0,-700); -- straight up
+			local Vector3 = Vector(0,0); -- dont need this but is needed as an arg
+			local Vector4 = Vector(0,0); -- dont need this but is needed as an arg		
+			self.ray = SceneMan:CastObstacleRay(self.Pos, Vector2, Vector3, Vector4, self.RootID, self.Team, 128, 7);
+			
+			self.rayTable = {self.ray};
+		end
+		
+		for _, rayLength in ipairs(self.rayTable) do
+			if rayLength < 0 then
+				outdoorRays = outdoorRays + 1;
+			elseif rayLength > 170 then
+				bigIndoorRays = bigIndoorRays + 1;
+			else
+				indoorRays = indoorRays + 1;
+			end
+		end
+		
+		if outdoorRays >= self.rayThreshold then
+			self.reflectionSound:Play(self.Pos);
+		end
+	end
+	
+	-- Animation + HUD
+	if self.parent then
+		
+		local ctrl = self.parent:GetController();
+		local screen = ActivityMan:GetActivity():ScreenOfPlayer(ctrl.Player);
+		if self.parent:IsPlayerControlled() and (ctrl:IsState(Controller.PIE_MENU_ACTIVE) or (self.grenadeHUDReady and not self.grenadeHUDTimer:IsPastSimMS(1000))) then
+			local pos = self.parent.AboveHUDPos + Vector(0, 24)
+			
+			if self.grenadeHUDReady and not ctrl:IsState(Controller.PIE_MENU_ACTIVE) and not self.grenadeHUDTimer:IsPastSimMS(2000) then
+				PrimitiveMan:DrawTextPrimitive(screen, pos + Vector(0, 10), "Nade Launcher Ready!", true, 1);
+			elseif self.grenadeLoaded then
+				PrimitiveMan:DrawTextPrimitive(screen, pos, "Underbarrel Nade: Ready", true, 1);
+				PrimitiveMan:DrawTextPrimitive(screen, pos + Vector(0, 10), "Press V to fire", true, 1);
+			else
+				PrimitiveMan:DrawTextPrimitive(screen, pos, "Underbarrel Nade: Loading...", true, 1);
+			end
+		end
+		
+		self.horizontalAnim = math.floor(self.horizontalAnim / (1 + TimerMan.DeltaTimeSecs * 24.0) * 1000) / 1000
+		self.verticalAnim = math.floor(self.verticalAnim / (1 + TimerMan.DeltaTimeSecs * 15.0) * 1000) / 1000
+		
+		local stance = Vector()
+		stance = stance + Vector(-1,0) * self.horizontalAnim -- Horizontal animation
+		stance = stance + Vector(0,5) * self.verticalAnim -- Vertical animation
+		
+		self.rotationTarget = self.rotationTarget - (self.angVel * 4)
+		
+		-- Progressive Recoil Update
+		if self.FiredFrame then
+			self.recoilStr = self.recoilStr + ((math.random(10, self.recoilRandomUpper * 10) / 10) * 0.5 * self.recoilStrength) + (self.recoilStr * 0.6 * self.recoilPowStrength)
+			self:SetNumberValue("recoilStrengthBase", self.recoilStrength * (1 + self.recoilPowStrength) / self.recoilDamping)
+		end
+		self:SetNumberValue("recoilStrengthCurrent", self.recoilStr)
+		
+		self.recoilStr = math.floor(self.recoilStr / (1 + TimerMan.DeltaTimeSecs * 8.0 * self.recoilDamping) * 1000) / 1000
+		self.recoilAcc = (self.recoilAcc + self.recoilStr * TimerMan.DeltaTimeSecs) % (math.pi * 4)
+		
+		local recoilA = (math.sin(self.recoilAcc) * self.recoilStr) * 0.05 * self.recoilStr
+		local recoilB = (math.sin(self.recoilAcc * 0.5) * self.recoilStr) * 0.01 * self.recoilStr
+		local recoilC = (math.sin(self.recoilAcc * 0.25) * self.recoilStr) * 0.05 * self.recoilStr
+		
+		local recoilFinal = math.max(math.min(recoilA + recoilB + recoilC, self.recoilMax), -self.recoilMax)
+		
+		self.SharpLength = math.max(self.originalSharpLength - (self.recoilStr * 3 + math.abs(recoilFinal)), 0)
+		
+		self.rotationTarget = self.rotationTarget + recoilFinal -- apply the recoil
+		-- Progressive Recoil Update		
+		
+		self.rotation = (self.rotation + self.rotationTarget * TimerMan.DeltaTimeSecs * self.rotationSpeed) / (1 + TimerMan.DeltaTimeSecs * self.rotationSpeed)
+		local total = math.rad(self.rotation) * self.FlipFactor
+		
+		self.RotAngle = self.RotAngle + total;
+		self:SetNumberValue("MagRotation", total);
+		
+		local jointOffset = Vector(self.JointOffset.X * self.FlipFactor, self.JointOffset.Y):RadRotate(self.RotAngle);
+		local offsetTotal = Vector(jointOffset.X, jointOffset.Y):RadRotate(-total) - jointOffset
+		self.Pos = self.Pos + offsetTotal;
+		
+		self.StanceOffset = Vector(self.originalStanceOffset.X, self.originalStanceOffset.Y) + stance
+		self.SharpStanceOffset = Vector(self.originalSharpStanceOffset.X, self.originalSharpStanceOffset.Y) + stance
+	end
+	
+	if self.canSmoke then	
+		local poof = CreateMOSParticle("Tiny Smoke Ball 1");
+		poof.Pos = self.Pos + Vector(self.MuzzleOffset.X * self.FlipFactor, self.MuzzleOffset.Y):RadRotate(self.RotAngle);
+		poof.Lifetime = poof.Lifetime * RangeRand(0.3, 1.3) * 0.9;
+		poof.Vel = self.Vel * 0.1
+		poof.GlobalAccScalar = RangeRand(0.9, 1.0) * -0.4; -- Go up and down
+		MovableMan:AddParticle(poof);
+	end
+end
